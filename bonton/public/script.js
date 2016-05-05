f082deea902f38749bdb81b2f26ecd510ac7559e
mfcastro <mfcastro50@gmail.com>
2016-05-05 16:24:38 -0500

    Added picture hover feature to the woman page

---
+++ bonton/public/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+	//$('.toggle').hover(function () {
+	//	//$(".toggle").toggle("bounce", { times: 1 }, 'slow');
+	//	$('.toggle').fadeOut(3000);
+	//});
+
+
+	//$('.toggle').mouseover(function () {
+	//	$(this).fadeOut(100);
+	//	//$(this).toggle("bounce", { times: 1 }, "slow");
+
+	//});
+
+	//$('.toggle').mouseover(function () {
+	//	$(this).fadeIn(200);
+	});
+			
+		});
